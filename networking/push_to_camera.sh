7d3fc53e60ddbaa87c3472cbfa6e848765158fff
wribbe <eng_steff@hotmail.com>
2016-11-30 12:47:20 +0100

    Separate compilation of local and embedded objects, add make push.

--- networking/push_to_camera.sh
+++ networking/push_to_camera.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 exec_dir=executables
-executable=${exec_dir}/"eda040_server"
+executable=${exec_dir}/embedded/"eda040_server"
 cam_num="2"
 address="argus-${cam_num}.e.lth.lu.se"
 scp_user="rt"
